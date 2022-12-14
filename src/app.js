3c40874e3121d02aa3e95805f9d04fdd4496250a
TEBE 852 <alaneidepimenta1997@gmail.com>
2022-12-14 11:23:08 -0300

    acrescentei indexRoutes

--- src/app.js
+++ src/app.js
@@ -5,11 +5,13 @@ const app = express()
 const database = require('./database/mongoConfig') 
 const rotas = require('./routes/voluntariaRoutes')
 const router = require ("./routes/beneficiadasRoutes")
+const routes = require ("./routes/indexRoutes")
 
 app.use(express.json()) 
 app.use(cors()) 
 app.use("/voluntaria", rotas);
 app.use("/beneficiadas", router);
+app.use(routes);
 
 
 
